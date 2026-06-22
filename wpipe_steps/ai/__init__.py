a3d3a878e9dd546fa767c3638e36a860d9ba0ecd
williamrv-edt <138199553+williamrv-edt@users.noreply.github.com>
2026-06-22 11:44:12 +0200

    Refactor: update main ai/__init__.py to import all subcategory steps

--- wpipe_steps/ai/__init__.py
+++ wpipe_steps/ai/__init__.py
@@ -1,9 +1,107 @@
-from .openai_prompt import OpenAiPromptStep
-from .huggingface_inference import HuggingFaceInferenceStep
-from .sentiment_analysis import SentimentAnalysisStep
+from .text import HFTextClassificationStep
+from .text import HFConversationalStep
+from .text import HFDocumentQuestionAnsweringStep
+from .text import HFFeatureExtractionStep
+from .text import HFFillMaskStep
+from .text import HuggingFaceInferenceStep
+from .text import HFLanguageIdentificationStep
+from .text import HFMultipleChoiceStep
+from .text import HFNerStep
+from .text import OpenAiPromptStep
+from .text import HFQuestionAnsweringStep
+from .text import HFRerankingStep
+from .text import HFSemanticSearchStep
+from .text import HFSentenceEmbeddingsStep
+from .text import HFSentenceSimilarityStep
+from .text import HFSentimentAnalysisStep
+from .text import SentimentAnalysisStep
+from .text import HFSummarizationStep
+from .text import HFTableQuestionAnsweringStep
+from .text import HFText2TextGenerationStep
+from .text import HFTextGenerationStep
+from .text import HFTranslationStep
+from .text import HFZeroShotClassificationStep
+from .vision import HFImageBackgroundRemovalStep
+from .vision import HFImageToTextStep
+from .vision import HFImageClassificationStep
+from .vision import HFImageColorizationStep
+from .vision import HFDepthEstimationStep
+from .vision import HFObjectDetectionStep
+from .vision import HFFaceDetectionStep
+from .vision import HFImageToImageStep
+from .vision import HFInpaintingStep
+from .vision import HFOcrStep
+from .vision import HFImageSegmentationStep
+from .vision import HFImageStyleTransferStep
+from .vision import HFImageSuperResolutionStep
+from .vision import HFVideoClassificationStep
+from .vision import HFVideoFrameInterpolationStep
+from .vision import HFVisualQuestionAnsweringStep
+from .vision import HFZeroShotImageClassificationStep
+from .audio import HFAutomaticSpeechRecognitionStep
+from .audio import HFAudioToAudioStep
+from .audio import HFAudioClassificationStep
+from .audio import HFSpeakerDiarizationStep
+from .audio import HFAudioEmotionRecognitionStep
+from .audio import HFSpeechToSpeechStep
+from .audio import HFTextToSpeechStep
+from .audio import HFVoiceActivityDetectionStep
+from .multimodal import HFAnyToAnyStep
+from .multimodal import HFDocumentVisualQuestionAnsweringStep
+from .multimodal import HFImageTextToTextStep
+from .multimodal import HFTableDetectionStep
 
 __all__ = [
-    "OpenAiPromptStep",
+    "HFTextClassificationStep",
+    "HFConversationalStep",
+    "HFDocumentQuestionAnsweringStep",
+    "HFFeatureExtractionStep",
+    "HFFillMaskStep",
     "HuggingFaceInferenceStep",
-    "SentimentAnalysisStep"
+    "HFLanguageIdentificationStep",
+    "HFMultipleChoiceStep",
+    "HFNerStep",
+    "OpenAiPromptStep",
+    "HFQuestionAnsweringStep",
+    "HFRerankingStep",
+    "HFSemanticSearchStep",
+    "HFSentenceEmbeddingsStep",
+    "HFSentenceSimilarityStep",
+    "HFSentimentAnalysisStep",
+    "SentimentAnalysisStep",
+    "HFSummarizationStep",
+    "HFTableQuestionAnsweringStep",
+    "HFText2TextGenerationStep",
+    "HFTextGenerationStep",
+    "HFTranslationStep",
+    "HFZeroShotClassificationStep",
+    "HFImageBackgroundRemovalStep",
+    "HFImageToTextStep",
+    "HFImageClassificationStep",
+    "HFImageColorizationStep",
+    "HFDepthEstimationStep",
+    "HFObjectDetectionStep",
+    "HFFaceDetectionStep",
+    "HFImageToImageStep",
+    "HFInpaintingStep",
+    "HFOcrStep",
+    "HFImageSegmentationStep",
+    "HFImageStyleTransferStep",
+    "HFImageSuperResolutionStep",
+    "HFVideoClassificationStep",
+    "HFVideoFrameInterpolationStep",
+    "HFVisualQuestionAnsweringStep",
+    "HFZeroShotImageClassificationStep",
+    "HFAutomaticSpeechRecognitionStep",
+    "HFAudioToAudioStep",
+    "HFAudioClassificationStep",
+    "HFSpeakerDiarizationStep",
+    "HFAudioEmotionRecognitionStep",
+    "HFSpeechToSpeechStep",
+    "HFTextToSpeechStep",
+    "HFVoiceActivityDetectionStep",
+    "HFAnyToAnyStep",
+    "HFDocumentVisualQuestionAnsweringStep",
+    "HFImageTextToTextStep",
+    "HFTableDetectionStep",
 ]
