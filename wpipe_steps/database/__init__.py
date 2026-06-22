2c1b2d17bd74751c2516ca3da5bc8fcd9ff8d363
williamrv-edt <138199553+williamrv-edt@users.noreply.github.com>
2026-06-22 11:44:12 +0200

    Refactor: update database/__init__.py with mongodb package name

--- wpipe_steps/database/__init__.py
+++ wpipe_steps/database/__init__.py
@@ -1,15 +1,15 @@
 from .mysql import MySQLQueryStep, mysql_query
 from .redis import RedisCacheStep
-from .mongo import MongoInsertStep, mongo_insert
+from .mongodb import MongoInsertStep, mongo_insert
 from .sqlite import SQLiteAuditStep, sqlite_audit
 from .clickhouse import ClickHouseBulkStep, clickhouse_bulk
 from .cassandra import CassandraWriteStep, cassandra_write
 
 __all__ = [
-    "MySQLQueryStep", 
+    "MySQLQueryStep",
     "mysql_query",
-    "RedisCacheStep", 
-    "MongoInsertStep", 
+    "RedisCacheStep",
+    "MongoInsertStep",
     "mongo_insert",
     "SQLiteAuditStep",
     "sqlite_audit",
